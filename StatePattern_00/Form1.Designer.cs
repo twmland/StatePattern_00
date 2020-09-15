8c80b74dce5ff4c73d9382bf96f3355301c90d1e
King Liu <ntutking@hotmail.com>
2020-09-15 11:19:12 +0800

    First

--- StatePattern_00/Form1.Designer.cs
+++ StatePattern_00/Form1.Designer.cs
@@ -29,35 +29,84 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.txtBxGrade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(797, 160);
+            this.button1.Location = new System.Drawing.Point(647, 78);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(312, 97);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBxGrade
+            // 
+            this.txtBxGrade.Location = new System.Drawing.Point(89, 30);
+            this.txtBxGrade.Name = "txtBxGrade";
+            this.txtBxGrade.Size = new System.Drawing.Size(234, 36);
+            this.txtBxGrade.TabIndex = 1;
+            this.txtBxGrade.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Grade:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Level:";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLevel.Location = new System.Drawing.Point(94, 101);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(228, 36);
+            this.lblLevel.TabIndex = 4;
+            this.lblLevel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1156, 600);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBxGrade);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBxGrade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLevel;
     }
 }
 
--- StatePattern_00/Form1.cs
+++ StatePattern_00/Form1.cs
@@ -16,5 +16,49 @@ namespace StatePattern_00
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int grade = 1;
+            try
+            {
+                grade = Convert.ToInt32(txtBxGrade.Text);
+            }
+            catch
+            {
+                txtBxGrade.Text = grade.ToString();
+
+            }
+        }
     }
+
+
+    public class User
+    {
+        public int Grade = 1;
+        public StateContext State;
+        public User()
+        {
+
+        }
+        public void SetState(StateContext state)
+        {
+            State = state;
+        }
+        public string GetLevelString()
+        {
+            return this.
+        }
+    }
+
+    public abstract class StateContext
+    {
+        
+        public abstract string GetLevelString();
+    } 
 }
